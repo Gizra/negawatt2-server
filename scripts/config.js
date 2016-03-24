7d09b51b3a43b7f82cbcd0ca8e3e6c5a082cbec1
Shahar <shahard@arava-ect.com>
2016-03-24 19:25:48 +0200

    Built negawattclient from commit 51ab1a2 on branch master-0.8

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://live-negawatt2.pantheonsite.io',cname:'negawatt2.gizra.com',debugUiRouter:false,tileLayerOptions:{id:'niryariv.o0okn67c',accessToken:'pk.eyJ1IjoibmlyeWFyaXYiLCJhIjoiQjdJeWdqZyJ9.pZOwn6stABGoptmp0DH1wg'},openweather:{endpoint:null,apikey:'9a67a4889f54dd2a86858f692bfbe62e'}})
+.constant('Config', {backend:'http://dev-negawatt2.pantheonsite.io',cname:'negawatt2-dev.gizra.com',debugUiRouter:false,tileLayerOptions:{id:'niryariv.o0okn67c',accessToken:'pk.eyJ1IjoibmlyeWFyaXYiLCJhIjoiQjdJeWdqZyJ9.pZOwn6stABGoptmp0DH1wg'},openweather:{endpoint:null,apikey:'9a67a4889f54dd2a86858f692bfbe62e'}})
 
 ;
